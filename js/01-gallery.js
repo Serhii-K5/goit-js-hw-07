0e102e2d39d961daa0b9320ef84af5a5fb07d827
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-03-06 14:14:53 +0200

    Solving HW7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const arr = [];
+
+galleryItems.forEach(el => {
+	const galleryDiv = document.createElement('div');
+	galleryDiv.className = 'gallery__item';
+
+	const link = document.createElement('a');
+	link.className = 'gallery__link';
+	link.href = el.original;
+
+	const img = document.createElement('img');
+  img.className = 'gallery__image';
+  img.src = el.preview;
+  img.setAttribute('data-source', el.original);
+  img.alt = el.description;
+
+  galleryDiv.append(link);
+	link.append(img);
+	arr.push(galleryDiv);
+})
+
+gallery.append(...arr);
+
+gallery.addEventListener('click', el => {  
+  el.preventDefault();
+
+  if (el.target.nodeName !== 'IMG') {return};
+
+  const selectedEl = el.target.getAttribute('data-source');
+  const imgLightbox = basicLightbox.create(`<img src="${selectedEl}">`);
+
+  imgLightbox.show();
+    
+  gallery.addEventListener('keydown', el => {
+		if (el.key === 'Escape') {imgLightbox.close()};
+	});
+});
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+const arr = [];
+
+galleryItems.forEach(el => {
+	const link = document.createElement('a');
+	link.className = 'gallery__link';
+	link.href = el.original;
+
+	const img = document.createElement('img');
+  img.className = 'gallery__image';
+  img.src = el.preview;
+  img.setAttribute('title', el.original);
+  img.alt = el.description;
+
+  link.append(img);
+	arr.push(link);
+});
+
+gallery.append(...arr);
+
+new SimpleLightbox('.gallery a', {});
+
 console.log(galleryItems);
