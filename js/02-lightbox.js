166b4b5e42c63a38b622af21b769f28fd6e3821f
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-03-23 10:06:48 +0200

    added the 2nd version of the task solution

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,24 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector('.gallery');
-const arr = [];
+// v1
+// const arr = [];
 
-galleryItems.forEach(el => {
-	const link = document.createElement('a');
-	link.className = 'gallery__link';
-	link.href = el.original;
+// galleryItems.map(el => {
+// 	const link = document.createElement('a');
+// 	link.className = 'gallery__link';
+// 	link.href = el.original;
 
-	const img = document.createElement('img');
-  img.className = 'gallery__image';
-  img.src = el.preview;
-  img.setAttribute('title', el.original);
-  img.alt = el.description;
+// 	const img = document.createElement('img');
+//   img.className = 'gallery__image';
+//   img.src = el.preview;
+//   img.setAttribute('title', el.original);
+//   img.alt = el.description;
 
-  link.append(img);
-	arr.push(link);
-});
+//   link.append(img);
+// 	arr.push(link);
+// });
 
-gallery.append(...arr);
+// gallery.append(...arr);
+
+// v2
+const textInsert = galleryItems
+	.map(el => `<a class="gallery__link" href=${el.original}><img class="gallery__image" src=${el.preview} data-source=${el.original} alt=${el.description}></a>`)
+	.join('');
+
+gallery.insertAdjacentHTML('beforeend', textInsert);
 
 new SimpleLightbox('.gallery a', {});
 
